0fa07fda999c44531f50892bf5b0eb4908ed1dd5
Carson Woods <cwoods289@gmail.com>
2019-03-08 11:40:11 -0500

    Update to adhere to python style conventions

--- LemonSpotter.py
+++ LemonSpotter.py
@@ -11,121 +11,164 @@ from pathlib import Path
 from src.element import element
 
 
-
-
-
-
 # Loads an element from JSON db
 # Returns an element object populated from jsong
 def load_element(db_path, name):
+    """
+    Crawls the loaded database and searches for entry that matches name
+
+    Paramters:
+    db_path (string) : Path to the database to crawl
+    name    (string) : Name of the element to be loaded
+
+    Returns:
+    element: Returns an element loaded with information from database
+    """
 
 	# Recursively loads all json files searching
 	# for a funciton with matching name
-	pathlist = Path(db_path).glob("**/*.json")
-	for path in pathlist:
-		try:
-			file = open(str(path))
-			json_obj = json.load(file)
+    pathlist = Path(db_path).glob("**/*.json")
+    for path in pathlist:
+        try:
+            file = open(str(path))
+            json_obj = json.load(file)
 
-			# Loads all parameters from JSON
-			element_name = json_obj["name"]
+            # Loads all parameters from JSON
+            element_name = json_obj["name"]
 
-			if element_name == name:
-				return_type = json_obj["return"]
-				parameters = json_obj["arguments"]
-				requires = json_obj["requires"]
-				return element(element_name, return_type, parameters, requires)
-		except:
-			print("Error when loading json file: " + str(path))
+            if element_name == name:
+                return_type = json_obj["return"]
+                parameters = json_obj["arguments"]
+                requires = json_obj["requires"]
+                return element(element_name, return_type, parameters, requires)
+        except ValueError:
+            print("Error when loading json file: " + str(path))
 
-	return element()
+    return element()
 
 
 
-# Runs the paramter command passed as a string. 
+# Runs the paramter command passed as a string.
 # Prints what would normally be output to terminal.
 # NOTE: commands must be run individually.
 def run_process(command):
-	command = command.split(" ");
-	process = subprocess.Popen(command, cwd="tests/", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-	stdout, stderr = process.communicate()
-	print(stdout)
-	print(stderr)
+    """
+    Runs a CLI process; used for running C test files.
+
+    Paramters:
+    command (string) : Command is the command that will be run
+    """
+    command = command.split(" ")
+    process = subprocess.Popen(command,
+                               cwd="tests/",
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
+
+    stdout, stderr = process.communicate()
+    print(stdout)
+    print(stderr)
 
 
-# Generates a test file that includes the MPI_Elements that are passed to it. 
+# Generates a test file that includes the MPI_Elements that are passed to it.
 def generate_test(file_name, elements=[]):
+    """
+    Autogenerates the C test files for each test.
 
-	# Generates test file 
-	f = open("tests/" + file_name, "w+")
+    Parameters:
+    file_name (string)   : Name of the file that will be created
+    elements  (string[]) : List of strings cooresponding to the elements to be included in test
+    """
 
-	# Sets up file for testing
-	f.write("#include \"mpi.h\"\n")
-	f.write("#include <stdio.h>\n")
-	f.write("#include <stdlib.h>\n\n\n")
-	f.write("int main(int argc, char** argv) {\n")
+    print(elements)
 
-	# Writes the MPI elements to C test file
+    # Generates test file
+    file = open("tests/" + file_name, "w+")
 
+    # Sets up file for testing
+    file.write("#include \"mpi.h\"\n")
+    file.write("#include <stdio.h>\n")
+    file.write("#include <stdlib.h>\n\n\n")
+    file.write("int main(int argc, char** argv) {\n")
 
-	f.write("return 0;\n")
-	f.write("}\n")
-	
-	f.close()
-
+    # Writes the MPI elements to C test file
 
 
+    file.write("return 0;\n")
+    file.write("}\n")
+    file.close()
 
-# Cleans up test after runtime
+
+
+
+
 def clean_test(file_name):
-	os.remove("tests/" + file_name)
+    """
+    Deletes the associated file_name from tests directory.
+
+    Paramters:
+    file_name (string) : The name of the file being deleted
+    """
+    os.remove("tests/" + file_name)
 
 
-''' 
-	Testname will be the name of the test case being tested
-	Testcases will be different scenarios of this testname. 
-	This could be MPI_Send() with different parameter combinations.
-	Results will store results of each test at its index in testcases list
-'''
-def log(testname, testcases = [], results=[]):
 
-	# Open log file for specific testcase
-	log_file = open("logs/" + testname + ".json", "w+")
-	
-	log_file.write("{\n\t\"" + testname + "\" : {\n")
-	for x in range(0, len(testcases)):
-		log_file.write("\t\t\"" + testcases[x] + "\" : {\n")
-		log_file.write("\t\t\t\"Result\" : \"" + results[x] + "\"\n")
-	
-		# Tests to see if on the last element on the list	
-		if (x < len(testcases)-1):
-			log_file.write("\t\t}, \n")
-		else:
-			log_file.write("\t\t}\n")
-	
-	log_file.write("\t}\n")
-	log_file.write("}")
+def log(testname, testcases=[], results=[]):
+    """
+    Logs output of all testcases for a specific test to a log file.
+
+    Parameters:
+    testname (string)   : String of type of test being run
+    tescases (string[]) : List of strings cooresponding to individual testcases
+    results  (string[]) : List of integers cooresponding to the results the testcases
+    """
+
+    # Open log file for specific testcase
+    log_file = open("logs/" + testname + ".json", "w+")
+
+    log_file.write("{\n\t\"" + testname + "\" : {\n")
+    for case, index in enumerate(testcases, 0):
+        log_file.write("\t\t\"" + case + "\" : {\n")
+        log_file.write("\t\t\t\"Result\" : \"" + results[index] + "\"\n")
+
+        # Tests to see if on the last element on the list
+        if index < len(testcases)-1:
+            log_file.write("\t\t}, \n")
+        else:
+            log_file.write("\t\t}\n")
+
+    log_file.write("\t}\n")
+    log_file.write("}")
 
 
 
 # Main runtime for LemonSpotter
 def main():
+    """
+    Main runtime for Lemonspotter.
+    """
 
-	# Parse CLI Arguments for more granular control
-	parser = argparse.ArgumentParser(description="Specify runtime variables", prog="Lemonspotter")
+    # Parse CLI Arguments for more granular control
+    parser = argparse.ArgumentParser(description="Specify runtime variables", prog="Lemonspotter")
 
-	parser.add_argument('-l', "--load", metavar="db_path", nargs='?', const="../lemonspotter-mpi1/mpi_1_0/", 
-						default="../lemonspotter-mpi1/mpi_1_0/", help="specify relative path to database", dest="load")
-	parser.add_argument('-v', "--version", help="print version of Lemonspotter", action='version', version="%(prog)s 0.1")
+    parser.add_argument('-l', "--load",
+                        metavar="db_path",
+                        nargs='?',
+                        const="../lemonspotter-mpi1/mpi_1_0/",
+                        default="../lemonspotter-mpi1/mpi_1_0/",
+                        help="specify relative path to database",
+                        dest="load")
 
-	db_path = parser.parse_args().load
-	
+    parser.add_argument('-v', "--version",
+                        help="print version of Lemonspotter",
+                        action='version',
+                        version="%(prog)s 0.1")
 
+    db_path = parser.parse_args().load
 
-
-	test_element = load_element(db_path, "MPI_Init")
-	print(test_element.get_arguments_list())
+    test_element = load_element(db_path, "MPI_Init")
+    print(test_element.get_arguments_list())
 
 
 if __name__ == "__main__":
-	main()
+    main()
