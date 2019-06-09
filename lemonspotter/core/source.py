b46100d1f16e3a5b6ede7b1116a7cb9d291f5c41
Carson Woods <cwoods289@gmail.com>
2019-06-09 10:31:23 -0600

    Add file writing to source object

--- lemonspotter/core/source.py
+++ lemonspotter/core/source.py
@@ -1,5 +1,31 @@
+import os
+
 class Source:
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
         self.source_lines = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @name.deleter
+    def name(self):
+        del self._name
+
+    def write(self):
+        if not os.path.isdir('../tests'):
+            os.makedirs("../tests")
+
+        file_name = self.name + ".c"
+        test_file = open("../tests/" + file_name, "w+")
+
+        for line in self.source_lines:
+            test_file.write(line+"\n")
+
+        test_file.close()
