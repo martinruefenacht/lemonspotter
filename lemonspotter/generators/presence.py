0f0d86d8f7ddc6fe1af623ed28fbad08c26829d6
Martin Ruefenacht <m.a.ruefenacht@gmail.com>
2019-06-14 11:42:03 -0600

    Added stub for PresenceGenerator

---
+++ lemonspotter/generators/presence.py
@@ -0,0 +1,13 @@
+"""
+This module defines the function presence test generator.
+"""
+
+import logging
+
+from core.database import Database
+
+class PresenceGenearator:
+    def __init__(self, database: Database):
+        self.database = database
+
+        raise NotImplementedError
