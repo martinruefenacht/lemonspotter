c3b44e7cdec3ceec22a4637b3630847993bad953
Carson Woods <cwoods289@gmail.com>
2019-06-21 08:37:21 -0600

    Seperate output to build/exec. Add support for only building/running certain tests

--- lemonspotter/executors/mpiexecutor.py
+++ lemonspotter/executors/mpiexecutor.py
@@ -4,47 +4,107 @@ from subprocess import Popen, PIPE
 
 class MPIExecutor:
     def __init__(self, test_directory='../tests/'):
-        self._test_directory = test_directory
-        self._results = {}
+        """
+        Initializes a test executor for MPI Libraries
+        """
+        self._test_directory = os.path.abspath(test_directory)
+        self._build_results = {}
+        self._exec_results = {}
 
     @property
-    def results(self):
-        return self._results
+    def build_results(self):
+        """
+        Returns build_results of a test
+        """
+        return self._build_results
 
-    @results.setter
-    def results(self, results):
-        self._results = results
+    @build_results.setter
+    def build_results(self, build_results):
+        """
+        Sets the build_results of a test
+        """
+        self._build_results = build_results
 
-    @results.deleter
-    def results(self):
-        del self._results
+    @build_results.deleter
+    def build_results(self):
+        """
+        Delets the build_results of a test
+        """
+        del self._build_results
+
+    @property
+    def exec_results(self):
+        """
+        Returns exec_results of a test
+        """
+        return self._exec_results
+
+    @exec_results.setter
+    def exec_results(self, exec_results):
+        """
+        Sets the exec_results of a test
+        """
+        self._exec_results = exec_results
+
+    @exec_results.deleter
+    def exec_results(self):
+        """
+        Delets the exec_results of a test
+        """
+        del self._exec_results
 
     @property
     def test_directory(self):
+        """
+        Gets the directory where tests will be found/executed in
+        """
         return self._test_directory
 
     @test_directory.setter
     def test_directory(self, test_directory):
+        """
+        Sets the directory where tests will be found/executed in
+
+        """
         self._test_directory = test_directory
 
     @test_directory.deleter
     def test_directory(self):
+        """
+        Deletes the directory where tests will be found/executed in
+        """
         del self._test_directory
 
-    def build(self, args=[]):
+    def build(self, tests=[], args=[]):
+        """
+        Builds a test into a runnable executable
+
+        tests: list of source(later test) objects that define the tests to run
+        """
         if not os.path.isdir(self.test_directory):
             os.makedirs(test_directory)
 
-        files = pathlib.Path(self.test_directory).glob('**/*.c')
+        if tests:
+            for test in tests:
+                # Generate MPICC command that compiles test excutable
+                file_name = test.name + ".c"
+                mpicc = ["mpicc", file_name] + args + ["-o", self.test_directory, test.name]
 
-        for test in files:
-            test_name = str(test)[len(self._test_directory): len(str(test))-2]
+                process = Popen(mpicc, shell=True, stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process.communicate()
+                self.build_results[test.name] = [str(stdout), str(stderr)]
 
-            # Generate MPICC command that compiles test excutable
-            mpicc = ["mpicc", str(test)] + args + ["-o", self.test_directory, test_name]
+        else:
+            files = pathlib.Path(self.test_directory).glob('**/*.c')
+            for test in files:
+                test_name = str(test)[len(self._test_directory): len(str(test))-2]
+                # Generate MPICC command that compiles test excutable
+                mpicc = ["mpicc", str(test)] + args + ["-o", self.test_directory, test_name]
 
-            process = Popen(mpicc, shell=True, stdout=PIPE, stderr=PIPE)
-            stdout, stderr = process.communicate()
+                process = Popen(mpicc, shell=True, stdout=PIPE, stderr=PIPE)
+                self.build_results[test_name] = [str(stdout), str(stderr)]
+
+        return self.build_results
 
     def run(self, args=[]):
 
@@ -57,6 +117,6 @@ class MPIExecutor:
             mpiexec = ["mpiexec"] + args + [self.test_directory + test_name]
             process = Popen(mpiexec, shell=True, stdout=PIPE, stderr=PIPE)
             stdout, stderr = process.communicate()
-            self.results[test_name] = [str(stdout), str(stderr)]
+            self.exec_results[test_name] = [str(stdout), str(stderr)]
 
-        return self.results
+        return self.exec_results
