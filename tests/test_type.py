53ff1b7059e93379515f3534785246648dccbc2a
Martin Ruefenacht <m.a.ruefenacht@gmail.com>
2019-08-23 21:13:13 -0500

    Testing travis tests

--- tests/test_type.py
+++ tests/test_type.py
@@ -2,16 +2,16 @@ from hypothesis import given, strategies as st
 
 from lemonspotter.core.type import Type
 
-type_info = st.fixed_dictionaries({
-    'name': st.text(),
-    'abstract_type': st.text(),
-    'base_type': st.booleans(),
-    'language_type': st.text(),
-    'print_specifier': st.text()
-    #'reference'
-    #'dereference'
-    })
-
-@given(json=st.none())
-def test_type_instantiation(self, json):
-    type = Type(json)
+#type_info = st.fixed_dictionaries({
+#    'name': st.text(),
+#    'abstract_type': st.text(),
+#    'base_type': st.booleans(),
+#    'language_type': st.text(),
+#    'print_specifier': st.text()
+#    #'reference'
+#    #'dereference'
+#    })
+#
+##@given(json=st.none())
+#def test_type_instantiation(self, json):
+#    type = Type(json)
